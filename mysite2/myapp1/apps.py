25e64645959e637e2b7709a272ca11f85750772e
Casvil17 <m.cassim3108@gmail.com>
2023-08-29 20:40:48 +0200

    added apps.py comments

--- mysite2/myapp1/apps.py
+++ mysite2/myapp1/apps.py
@@ -2,5 +2,23 @@ from django.apps import AppConfig
 
 
 class Myapp1Config(AppConfig):
+    """
+    Configuration class for the 'myapp1' Django app.
+
+    This class defines the configuration for the 'myapp1' app. It specifies the
+    default auto field for models and sets the name of the app.
+
+    Attributes:
+        default_auto_field (str): The default auto field to use for models.
+            Set to 'django.db.models.BigAutoField' to use a big integer as the
+            auto field for model primary keys.
+        name (str): The name of the app. Should match the name used in the
+            app's 'INSTALLED_APPS' configuration in Django settings.
+
+    Example:
+        To use this configuration in your Django project's settings, add
+        'myapp1.apps.Myapp1Config' to the 'INSTALLED_APPS' list.
+
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'myapp1'
