3be464639d5f3e3d6a1348e45ef47a9ac0b9d8ec
Casvil17 <m.cassim3108@gmail.com>
2023-08-29 16:16:20 +0200

    added comments

--- mysite2/myapp1/views.py
+++ mysite2/myapp1/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    """
+    This view handles the home-page functionality.
+
+    :param: request (HttpRequest): The HTTP request object.
+    :param: “index.html”: the file that has the desired context
+
+    Returns:
+        HttpResponse: The HTTP response for the view.
+    """
+
     return render(request, "index.html")
+
 
 def party(request):
     return render(request, 'party.html')
